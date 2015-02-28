007be5aa11fb6bec4c3a174ccae1cdd7132c9a5d
LonghuaWan <longhuawan@gmail.com>
2015-02-28 16:37:44 +0800

    js dialog

---
+++ js/dialog.js
@@ -0,0 +1,17 @@
+$("#dialog").dialog({
+   autoOpen: false,
+   modal: true,
+   buttons : {
+        "Confirm" : function() {
+            alert("You have confirmed!");            
+        },
+        "Cancel" : function() {
+          $(this).dialog("close");
+        }
+      }
+    });
+
+$("#callConfirm").on("click", function(e) {
+    e.preventDefault();
+    $("#dialog").dialog("open");
+});
