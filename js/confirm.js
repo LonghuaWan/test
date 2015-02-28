19df1c7a036a7e4dc09d57b90471068b06a0eafd
LonghuaWan <longhuawan@gmail.com>
2015-02-28 16:31:49 +0800

    js

---
+++ js/confirm.js
@@ -0,0 +1,19 @@
+$('<div></div>').appendTo('body')
+  .html('<div><h6>Yes or No?</h6></div>')
+  .dialog({
+      modal: true, title: 'message', zIndex: 10000, autoOpen: true,
+      width: 'auto', resizable: false,
+      buttons: {
+          Yes: function () {
+              doFunctionForYes();
+              $(this).dialog("close");
+          },
+          No: function () {
+              doFunctionForNo();
+              $(this).dialog("close");
+          }
+      },
+      close: function (event, ui) {
+          $(this).remove();
+      }
+});
